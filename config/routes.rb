117e652e4af19787b76a2960a1a16224e2e2d081
hayato_ubuntu_desktop <pisces2336@gmail.com>
2022-11-07 22:09:29 +0900

    [Update] userのrouting変更 (resourcesを使わず, id不要でshowのみに飛ばす)

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root to: "homes#top"
-  resources :users
+  get "/user", to: "users#show", as: "user"
   resources :commitions do
     resources :works
   end
