6e795e9202b9126411788fd868642156ec56e23c
hayato_ubuntu_desktop <pisces2336@gmail.com>
2022-11-13 10:40:35 +0900

    [Update] 「本日の作業」の対象を本日以前の作業全部に変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
-    @works_today = Work.where("date = ?", Date.today)
+    @works_today = Work.where("date <= ?", Date.today).order("date")
     @works_all = Work.all.order("date")
   end
 end
