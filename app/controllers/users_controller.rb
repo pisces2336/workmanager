e01d5bcce69cd4a9e76e073418cfbfb1dd05874f
hayato_ubuntu_desktop <pisces2336@gmail.com>
2022-12-31 18:02:34 +0900

    [Update] 作業一覧を作業名でもソートするよう変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
-    @works_today = Work.where("date <= ?", Date.today).order("date")
-    @works_all = Work.all.order("date")
+    @works_today = Work.where("date <= ?", Date.today).order("title").order("date")
+    @works_all = Work.all.order("title").order("date")
   end
 end
